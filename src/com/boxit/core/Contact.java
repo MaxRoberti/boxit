636cc33c7c031f98962fb526a424221dadc1bfe2
maximilienroberti <maximilienroberti@hotmail.com>
2014-01-22 12:49:39 +0100

    mise a jour contact

--- src/com/boxit/core/Contact.java
+++ src/com/boxit/core/Contact.java
@@ -1,5 +1,24 @@
 package com.boxit.core;
 
 public class Contact {
+	
+
+	private String pseudo;
+	
+	
+	public Contact(String pseudo)
+	{
+	this.pseudo= pseudo;
+	}
+	
+	private String getPseudo()
+	{
+		return pseudo;
+	}
+	
+	public void setPseudo(String pseudo)
+	{
+		this.pseudo=pseudo;
+	}
 
 }
